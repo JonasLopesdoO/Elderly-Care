8faa0d395310d1c3e016ec0a96c44507d2b259c2
Jonas Lopes <jonas.lopes@osf-global.com>
2019-11-17 00:50:18 -0300

    incluindo biblioteca para double click listener
    restando apenas as notificações

--- app/src/main/java/com/ufc/quixada/elderlycare/IdosoNotificarActivity.java
+++ app/src/main/java/com/ufc/quixada/elderlycare/IdosoNotificarActivity.java
@@ -1,15 +1,26 @@
 package com.ufc.quixada.elderlycare;
 
 import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import com.google.firebase.database.DatabaseReference;
+import com.pedromassango.doubleclick.DoubleClick;
+import com.pedromassango.doubleclick.DoubleClickListener;
 
 public class IdosoNotificarActivity extends AppCompatActivity {
 
+    LinearLayout layoutIdosoMain;
     TextView txtCodigoIdoso;
+    Integer codigoIdoso;
+    DatabaseReference idosoCuidadorRef = ConfiguracaoFirebase.getFirebase().child("idoso-cuidador");
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,11 +28,31 @@ public class IdosoNotificarActivity extends AppCompatActivity {
 
         txtCodigoIdoso = findViewById(R.id.txtCodigoIdoso);
         getSharedPreferences();
+        layoutIdosoMain = findViewById(R.id.layoutIdosoMain);
+
+        layoutIdosoMain.setOnClickListener(new DoubleClick(new DoubleClickListener() {
+            @Override
+            public void onSingleClick(View view) {
+                idosoCuidadorRef.child(String.valueOf(codigoIdoso)).setValue(1);
+            }
+
+            @Override
+            public void onDoubleClick(View view) {
+                idosoCuidadorRef.child(String.valueOf(codigoIdoso)).setValue(2);
+            }
+        }, 500));
+        layoutIdosoMain.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                idosoCuidadorRef.child(String.valueOf(codigoIdoso)).setValue(3);
+                return true;
+            }
+        });
     }
 
     protected void getSharedPreferences() {
         Preferencias preferencias = new Preferencias(IdosoNotificarActivity.this);
-        Integer codigoIdoso = preferencias.getCodigoIdoso();
+        codigoIdoso = preferencias.getCodigoIdoso();
         txtCodigoIdoso.setText(String.valueOf(codigoIdoso));
     }
 
